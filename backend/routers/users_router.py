b508124b5bffd878a62e4ae75eb3f02822233026
DeyanTotin <138721028+DeyanTotin@users.noreply.github.com>
2023-12-02 14:07:16 +0200

    Ordered the endpoints for player_profile_router.py and users_router.py

--- backend/routers/users_router.py
+++ backend/routers/users_router.py
@@ -45,29 +45,6 @@ def login(email: EmailStr = Query(),
     else:
         raise HTTPException(status_code=400, detail="Invalid login data.")
 
-#  ............There is no need for this functionality at this time............
-# @users_router.get('/')
-# def all_users(x_token: str = Header(default=None)):
-#     """ Used for admins to see a list with all users.
-#
-#     Args:
-#         - JWT token
-#
-#     Returns:
-#         - list of users(id, username, role)
-#     """
-#
-#     if x_token is None:
-#         raise HTTPException(status_code=401, detail="You must be logged in to view a list with users.")
-#
-#     user = get_user_or_raise_401(x_token)
-#
-#     if not user_service.is_admin(user):
-#         raise HTTPException(status_code=401, detail="Only admins can view a list with all users.")
-#
-#     return user_service.all_users()
-
-
 @users_router.get('/{id}')
 def user_info(id: int, x_token: str = Header(default=None)):
     """ Used for admins to see data information about a user.
@@ -92,66 +69,6 @@ def user_info(id: int, x_token: str = Header(default=None)):
         raise HTTPException(status_code=404, detail=f"User with id {id} does not exist.")
 
     return user_service.find_by_id_admin(id)
-
-
-@users_router.put('/edit/{id}')
-def edit_users_role(new_user: User, id: int, x_token: str = Header(default=None)):
-    """ Used for editing a user's role through user.id. Only admins can edit it.
-
-    Args:
-        - user.id: int(URL link)
-        - JWT token
-
-    Returns:
-        - Edited user
-    """
-
-    if x_token is None:
-        raise HTTPException(status_code=401, detail="You must be logged in and be an admin to edit users roles.")
-
-    user = get_user_or_raise_401(x_token)
-
-    if not user_service.is_admin(user):
-        raise HTTPException(status_code=401, detail="Only admins can edit roles.")
-
-    if not utilities.id_exists(id, 'users'):
-        raise HTTPException(status_code=404, detail=f"User with id {id} does not exist.")
-
-    if new_user.user_type != Role.ADMIN and new_user.user_type != Role.USER:
-        raise HTTPException(status_code=404, detail="Unknown role.")
-
-    old_user = find_by_id(id)
-
-    return user_service.edit_user_type(old_user, new_user)
-
-
-@users_router.delete('/delete/{id}')
-def delete_user(id: int, x_token: str = Header(default=None)):
-    """ Used for deleting a user through user.id. Only admins can delete it.
-
-    Args:
-        - user.id: int(URL link)
-        - JWT token
-
-    Returns:
-        - Deleted user
-    """
-
-    if x_token is None:
-        raise HTTPException(status_code=401, detail="You must be logged in and be an admin to delete a user.")
-
-    user = get_user_or_raise_401(x_token)
-
-    if not utilities.id_exists(id, 'users'):
-        raise HTTPException(status_code=404, detail=f"User with id {id} does not exist.")
-
-    if user_service.is_admin(user):
-        user_service.delete_user(id)
-
-    if not user_service.is_admin(user):
-        raise HTTPException(status_code=401, detail="You must be admin to delete a user.")
-
-    return {'User deleted.'}
 
 
 @users_router.post("/link-request")
@@ -295,3 +212,62 @@ def deny_promotion_request_endpoint(promote_request_id: int, x_token: str = Head
         raise HTTPException(status_code=403, detail="Only admins can deny promotion requests.")
 
     return user_service.deny_promote_request(promote_request_id)
+
+
+@users_router.put('/edit/{id}')
+def edit_users_role(new_user: User, id: int, x_token: str = Header(default=None)):
+    """ Used for editing a user's role through user.id. Only admins can edit it.
+
+    Args:
+        - user.id: int(URL link)
+        - JWT token
+
+    Returns:
+        - Edited user
+    """
+
+    if x_token is None:
+        raise HTTPException(status_code=401, detail="You must be logged in and be an admin to edit users roles.")
+
+    user = get_user_or_raise_401(x_token)
+
+    if not user_service.is_admin(user):
+        raise HTTPException(status_code=401, detail="Only admins can edit roles.")
+
+    if not utilities.id_exists(id, 'users'):
+        raise HTTPException(status_code=404, detail=f"User with id {id} does not exist.")
+
+    if new_user.user_type != Role.ADMIN and new_user.user_type != Role.USER:
+        raise HTTPException(status_code=404, detail="Unknown role.")
+
+    old_user = find_by_id(id)
+
+    return user_service.edit_user_type(old_user, new_user)
+
+@users_router.delete('/delete/{id}')
+def delete_user(id: int, x_token: str = Header(default=None)):
+    """ Used for deleting a user through user.id. Only admins can delete it.
+
+    Args:
+        - user.id: int(URL link)
+        - JWT token
+
+    Returns:
+        - Deleted user
+    """
+
+    if x_token is None:
+        raise HTTPException(status_code=401, detail="You must be logged in and be an admin to delete a user.")
+
+    user = get_user_or_raise_401(x_token)
+
+    if not utilities.id_exists(id, 'users'):
+        raise HTTPException(status_code=404, detail=f"User with id {id} does not exist.")
+
+    if user_service.is_admin(user):
+        user_service.delete_user(id)
+
+    if not user_service.is_admin(user):
+        raise HTTPException(status_code=401, detail="You must be admin to delete a user.")
+
+    return {'User deleted.'}
